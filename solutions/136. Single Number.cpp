8c768d7bc4ae4f5fe4fe1093ca76f86d391ebb1d
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-01-10 21:23:05 +0530

    32 ms

---
+++ solutions/136. Single Number.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+//     int singleNumber(vector<int>& nums) {
+//         unordered_map<int,int> mpp;
+        
+//         for(int i = 0 ; i < nums.size() ; i++){
+//             mpp[nums[i]]++;        
+//         }
+        
+//         for(int i = 0 ; i < nums.size() ; i++ ){
+            
+//             if(mpp[nums[i]] == 1){
+//                 return nums[i];
+//             }
+            
+//         }
+      
+//         return -1;
+//     }
+    
+    int singleNumber(vector<int>& nums) { 
+     
+      int ans  = 0;
+      
+        for(int i=0;i<nums.size();i++){
+            ans  ^= nums[i] ;
+        }
+        return ans;
+     }
+        
+};
