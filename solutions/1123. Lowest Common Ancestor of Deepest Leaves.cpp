cac48b47f516e089f7bda7b14515ef2a827b563f
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-01-10 21:21:50 +0530

    24 ms

---
+++ solutions/1123. Lowest Common Ancestor of Deepest Leaves.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    int getDepth(TreeNode* root) {
+        if (root == NULL) return 0;
+        int ld = getDepth(root ->left);
+        int rd = getDepth(root ->right);
+        
+        return 1 + max(ld , rd);
+    }
+    
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+         if(!root) return {};
+        
+        int left = getDepth(root ->left);
+        int right = getDepth(root ->right);
+        
+        if(left == right) return root;
+        
+        if(left > right) return lcaDeepestLeaves(root->left);
+        else return lcaDeepestLeaves(root->right);
+    }
+};
