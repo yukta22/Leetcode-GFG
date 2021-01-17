0e014b8e7d6b524c902d9eca38789e9055187d13
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-01-17 21:44:21 +0530

    4 ms

--- solutions/1641. Count Sorted Vowel Strings.cpp
+++ solutions/1641. Count Sorted Vowel Strings.cpp
@@ -1,20 +1,7 @@
 class Solution {
 public:
     int countVowelStrings(int n) {
-        int a = 1;
-        int e = 1;
-        int i = 1;
-        int o = 1;
-        int u = 1;
-        
-        while(--n){
-            e += a;
-            i += e;
-            o += i;
-            u += o;
-        }
-     
-        return a + e + i + o + u;
+        return (n + 1) * (n + 2) * (n + 3) * (n + 4) / 24;
         
     }
 };
