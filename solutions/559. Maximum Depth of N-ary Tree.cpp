6a0ea2b9362a0d196975ee1fdc5f0a5560c9fb59
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-01-31 23:12:32 +0530

    28 ms

--- solutions/559. Maximum Depth of N-ary Tree.cpp
+++ solutions/559. Maximum Depth of N-ary Tree.cpp
@@ -1,26 +1,26 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    vector<Node*> children;
-​
-    Node() {}
-​
-    Node(int _val) {
-        val = _val;
-    }
-​
-    Node(int _val, vector<Node*> _children) {
-        val = _val;
-        children = _children;
-    }
-};
-*/
-​
 class Solution {
 public:
- int maxDepth(Node* root) {
+    int maxi =0;
+​
+    void helper(Node * root , int level){
+        if(!root) return;
+        
+        maxi = max(maxi,level);   
+        for(auto n : root->children){
+            helper(n,level+1);
+        }        
+    }
+​
+    int maxDepth(Node* root) {
+            
+         helper(root,1);
+         return maxi;   
+​
+    }
+};
+​
+/******* BFS 
+    int maxDepth(Node* root) {
      queue<Node*>q;
      int d = 0;
      
@@ -33,7 +33,7 @@ public:
          for(int i=0;i<n;i++){
              Node * curr = q.front();
              q.pop();
-             for(auto  child : curr->children){
+             for(auto  child : curr->children){
                  q.push(child);
              }
          }
@@ -41,27 +41,10 @@ public:
      }
    return d;
          
-  }
-    
-    
-    
-};
-/******* dFS ******/
-//  int maxi =0;
+  }
+******/
 ​
-//     void helper(Node * root , int level){
-//         if(!root) return;
-        
-//         maxi = max(maxi,level);
-        
-//         for(auto n : root->children){
-//             helper(n,level+1);
-//         }        
-//     }
 ​
-//     int maxDepth(Node* root) {
-            
-//          helper(root,1);
-//          return maxi;   
 ​
-//     }
+​
+​
