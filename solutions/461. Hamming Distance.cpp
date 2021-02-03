fb7446be26069ae2f6b2142eac3eafdfc20de371
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-02-03 18:27:51 +0530

    0 ms

---
+++ solutions/461. Hamming Distance.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        
+        int z = x^y;
+        int count =0;
+        
+        while(z){
+            
+            z &= (z-1);
+            count++;
+            
+        }
+        
+        return count;
+        
+    }
+};
