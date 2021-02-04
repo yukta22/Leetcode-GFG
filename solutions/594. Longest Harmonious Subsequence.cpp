1b831ebf655ef8f5a737c7e7d4f26dc2df35736f
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-02-04 18:40:15 +0530

    80 ms

--- solutions/594. Longest Harmonious Subsequence.cpp
+++ solutions/594. Longest Harmonious Subsequence.cpp
@@ -1,34 +1,55 @@
 class Solution {
 public:
+    
+    // time == O(n)  space == O(n)
+    
     int findLHS(vector<int>& nums) {
         
-        if(nums.size() == 0) return 0;
+        unordered_map<int,int>mp;
         
-        map<int,int> mpp;
-        
-        for(int i : nums){
-            mpp[i]++;
+        for(auto i : nums){
+            mp[i]++;
         }
         
-        int maxLHS=0;
-        auto it = mpp.begin();
-        int currentKey=it->first ,  freq=it->second;
+        int maxl = 0;
         
-        for(it = mpp.begin() ; it!=mpp.end() ; it++){
-            
-            if(currentKey + 1 == it->first){
-                
-                maxLHS = max(maxLHS , freq + it->second);
-                
-            }
-            
-            freq=it->second;
-            currentKey=it->first ;
-            
-            
+        for(auto & [num,f] : mp){
+           if(mp.find(num+1) != mp.end()){
+               maxl = max(maxl, f + mp[num +1] );
+           }
         }
         
-        return maxLHS;
-        
+        return maxl;
     }
 };
+​
+// class Solution {
+// public:
+//     int findLHS(vector<int>& nums) {
+        
+//         if(nums.size() == 0) return 0;
+        
+//         map<int,int> mpp;
+        
+//         for(int i : nums){
+//             mpp[i]++;
+//         }
+        
+//         int maxLHS=0;
+//         auto it = mpp.begin();
+//         int currentKey=it->first ,  freq=it->second;
+        
+//         for(it = mpp.begin() ; it!=mpp.end() ; it++){
+            
+//             if(currentKey + 1 == it->first){
+                
+//                 maxLHS = max(maxLHS , freq + it->second);
+                
+//             }
+            
+//             freq=it->second;
+//             currentKey=it->first ;
+            
+            
+//         }
+        
