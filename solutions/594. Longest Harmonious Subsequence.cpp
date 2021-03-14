f56229884b93bead3fcbea07ab818a434ba7759b
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-03-14 22:26:44 +0530

    72 ms

--- solutions/594. Longest Harmonious Subsequence.cpp
+++ solutions/594. Longest Harmonious Subsequence.cpp
@@ -15,7 +15,7 @@ public:
         
         for(auto & [num,f] : mp){
            if(mp.find(num+1) != mp.end()){
-               maxl = max(maxl, f + mp[num +1] );
+               maxl = max(maxl , mp[num] + mp[num +1] );
            }
         }
         
@@ -23,33 +23,6 @@ public:
     }
 };
 ​
-// class Solution {
-// public:
-//     int findLHS(vector<int>& nums) {
-        
-//         if(nums.size() == 0) return 0;
-        
-//         map<int,int> mpp;
-        
-//         for(int i : nums){
-//             mpp[i]++;
-//         }
-        
-//         int maxLHS=0;
-//         auto it = mpp.begin();
-//         int currentKey=it->first ,  freq=it->second;
-        
-//         for(it = mpp.begin() ; it!=mpp.end() ; it++){
-            
-//             if(currentKey + 1 == it->first){
-                
-//                 maxLHS = max(maxLHS , freq + it->second);
-                
-//             }
-            
-//             freq=it->second;
-//             currentKey=it->first ;
-            
-            
-//         }
-        
+​
+// public class Solution {
+// TC:o(n2)
