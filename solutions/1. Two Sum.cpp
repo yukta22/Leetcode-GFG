62978ef381fcdee43a881b55248db457867d42df
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-05-30 18:12:05 +0530

    12 ms

--- solutions/1. Two Sum.cpp
+++ solutions/1. Two Sum.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        
         vector<int> ans;
         unordered_map<int,int> mpp;
         
-        for(int i = 0; i < nums.size() ; i++){
+        for(int i = 0 ; i < nums.size() ; i++){
             if(mpp.find(target - nums[i]) != mpp.end()){
-                
                 ans.push_back(mpp[target - nums[i]]);
                 ans.push_back(i);
-                
                 return ans;
                 
             }
@@ -17,6 +16,6 @@ public:
             mpp[nums[i]] = i;
         }
         
-     return ans;   
+       return ans;
     }
 };
