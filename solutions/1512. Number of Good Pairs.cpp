95c2460acfd68b4f56d4af617c7e019dbd886048
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-05-31 10:31:05 +0530

    0 ms

--- solutions/1512. Number of Good Pairs.cpp
+++ solutions/1512. Number of Good Pairs.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
-    unordered_map<int,int>map;
-    int count = 0;
- 
-       
-    for(int i=0;i<nums.size();i++){
-      map[nums[i]]++;       
-    }  
-       
-    for(auto x : map){
-        count += x.second * (x.second-1)/2;
-    }   
-   
+        int count = 0;
+        
+        for(int i = 0 ; i < nums.size() ;i++){
+            for(int j = i+1 ; j < nums.size() ; j++ ){
+                if(nums[j] == nums[i]) count++;
+            }
+        }
         return count;
-      } 
- 
+    }
+    
 };
