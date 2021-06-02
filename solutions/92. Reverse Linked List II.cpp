2876f6b591b240c3c2674cea9c8a8fc9a1441cbf
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-06-02 21:21:59 +0530

    0 ms

--- solutions/92. Reverse Linked List II.cpp
+++ solutions/92. Reverse Linked List II.cpp
@@ -1,37 +1,29 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* reverseBetween(ListNode* head, int m, int n) {
-        
-       ListNode* h = head;
-        for (int i = 0; i < n - m; i++) {
-            int k1 = m + i;
-            int k2 = n - i;
-            if (k1 >= k2) return head;
-            ListNode* p = h;
-            ListNode* q = h;
-            while (k1 - 1 > 0) { 
-                p = p->next; 
-                k1--;
+    ListNode* reverseBetween(ListNode* head, int m, int n) {       
+    
+        for(int i = 0 ; i < n - m ; i++){
+            int d1 = m + i;
+            int d2 = n - i;            
+            if(d1 >= d2 )  return head;         
+                     
+            ListNode* curr1 = head;
+            ListNode* curr2 = head;
+                        
+            while((d1 - 1 )> 0) {
+                curr1 = curr1 -> next;
+                d1--;
             }
-            while (k2 - 1 > 0) {
-                q = q->next;
-                k2--;
+            while((d2 - 1) > 0) {
+                curr2 = curr2 -> next;
+                d2--;
             }
-            // swap
-            int tmp = p->val;
-            p->val = q->val;
-            q->val = tmp;
-        }
-        return head;
+             
+            int temp = curr1->val;
+            curr1->val=curr2->val;
+            curr2->val = temp;
+        } 
+          return head;
     }
 };
+​
