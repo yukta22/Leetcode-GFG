5b0fd78ffdbedeaa8bfeb58acc060c2c2fcf335c
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-06-04 22:34:53 +0530

    24 ms

--- solutions/559. Maximum Depth of N-ary Tree.cpp
+++ solutions/559. Maximum Depth of N-ary Tree.cpp
@@ -1,50 +1,26 @@
 class Solution {
 public:
-    int maxi =0;
-​
-    void helper(Node * root , int level){
-        if(!root) return;
-        
-        maxi = max(maxi,level);   
-        for(auto n : root->children){
-            helper(n,level+1);
-        }        
-    }
-​
     int maxDepth(Node* root) {
+        queue<Node*> q;
+        int d = 0;
+        
+        if(!root) return NULL; 
+        
+        q.push(root);
+        while(!q.empty()){
+            int n = q.size();
             
-         helper(root,1);
-         return maxi;   
-​
+            for(int i = 0 ; i < n ; i++){
+                Node* curr = q.front();
+                q.pop();
+                
+                for(auto child : curr->children){
+                    q.push(child);
+                }
+                
+            }
+            d++;
+        }
+        return d;
     }
 };
-​
-/******* BFS 
-    int maxDepth(Node* root) {
-     queue<Node*>q;
-     int d = 0;
-     
-     if(!root) return NULL;
-     
-     q.push(root);
-     while(!q.empty()){
-         int n = q.size();
-         
-         for(int i=0;i<n;i++){
-             Node * curr = q.front();
-             q.pop();
-             for(auto  child : curr->children){
-                 q.push(child);
-             }
-         }
-         d++;
-     }
-   return d;
-         
-  }
-******/
-​
-​
-​
-​
-​
