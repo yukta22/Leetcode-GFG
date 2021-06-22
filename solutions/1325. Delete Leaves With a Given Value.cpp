c143eb37dfbec3e0ce0403df82d8752562decccf
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-06-22 17:40:36 +0530

    12 ms

--- solutions/1325. Delete Leaves With a Given Value.cpp
+++ solutions/1325. Delete Leaves With a Given Value.cpp
@@ -13,14 +13,15 @@ class Solution {
 public:
     TreeNode* removeLeafNodes(TreeNode* root, int target) {
         if(root == NULL) return NULL;
-                
-        root->left = removeLeafNodes(root->left , target);
-        root->right = removeLeafNodes(root->right , target);
+        
+        root->left = removeLeafNodes(root->left,target);
+        root->right = removeLeafNodes(root->right,target);
         
         if(root->val == target && root->left == NULL && root->right == NULL){
             return NULL;
         }
         
         return root;
+        
     }
 };
