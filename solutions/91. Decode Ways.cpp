5662e9d1107dee4c1f028c18c99d5ac95d98b929
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-07-14 21:58:29 +0530

    0 ms

--- solutions/91. Decode Ways.cpp
+++ solutions/91. Decode Ways.cpp
@@ -1,25 +1,48 @@
+// *****************memoization ***************
+// class Solution {
+// public:
+//     int dp[101];
+//     int helper(int i , string s){
+//         if(i >= s.size()) return 1;
+//         if(dp[i] != -1) return dp[i];
+//         int ans = 0 ;
+//         int op1 = s[i] - '0',op2=0;
+        
+//         if(i < s.size()-1){
+//             op2 = op1*10 + s[i+1] - '0';
+//         }        
+//         if(op1 > 0)  ans += helper(i+1,s);
+//         if(op1>0 && op2>0 && op2<=26) ans+=helper(i+2,s);
+        
+//         return dp[i] = ans;
+//     }  
+//     int numDecodings(string s) {
+//         memset(dp , -1,sizeof dp);
+//         return helper(0,s);
+//     }
+// };
+​
+// *****************dp ***************
 class Solution {
 public:
-    int dp[101];
-    int helper(int i , string s){
-        if(i >= s.size()) return 1;
-        if(dp[i] != -1) return dp[i];
-        int ans = 0 ;
-        int op1 = s[i] - '0',op2=0;
-        
-        if(i < s.size()-1){
-            op2 = op1*10 + s[i+1] - '0';
-        }
-        
-        if(op1 > 0)  ans += helper(i+1,s);
-        if(op1>0 && op2>0 && op2<=26) ans+=helper(i+2,s);
-        
-        return dp[i] = ans;
-        
-    }
-    
     int numDecodings(string s) {
-        memset(dp , -1,sizeof dp);
-        return helper(0,s);
+        int dp[102];
+        memset(dp ,0,sizeof dp);
+        int n = s.size();
+        dp[n+1] = 1;
+        dp[n] = 1;
+        for(int i = n-1 ;i>= 0 ;i--){
+            int op1 = s[i] - '0',op2=0;
+        
+            if(i < s.size()-1){
+                op2 = op1*10 + s[i+1] - '0';
+            }        
+            if(op1 > 0) dp[i] += dp[i+1];
+            if(op1>0 && op2>0 && op2<=26)  dp[i] += dp[i+2];
+        }
+        return dp[0];
     }
 };
+​
+​
+​
