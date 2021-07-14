4da4794f501f1e28f0987912b09169701017a88a
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-07-14 21:17:21 +0530

    4 ms

--- solutions/53. Maximum Subarray.cpp
+++ solutions/53. Maximum Subarray.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int sum = 0 ;
+        int n = nums.size();        
+        int dp[n];
+        
+        dp[0] = nums[0];
         int maxi = nums[0];
         
-        for(int i = 0 ; i < nums.size() ; i++){
-            
-            sum += nums[i];
-            maxi = max(sum , maxi);
-            
-            if(sum < 0) sum = 0 ;
-            
+        for(int i = 1 ;i < n ; i++){
+            dp[i] = max(nums[i] , nums[i] + dp[i-1]);
+            maxi = max(maxi , dp[i]);
         }
-        
         return maxi;
+        
     }
 };
