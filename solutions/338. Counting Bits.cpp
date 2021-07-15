cab102a48218df662b9770cd40bcb27961ed3222
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-07-15 21:30:35 +0530

    4 ms

--- solutions/338. Counting Bits.cpp
+++ solutions/338. Counting Bits.cpp
@@ -1,3 +1,19 @@
+// class Solution {
+// public:
+//     vector<int> countBits(int n) {
+//         if(n == 0) return {0};
+//         vector<int> dp(n+1,0);
+        
+//         dp[0] = 0;
+//         dp[1] = 1;
+        
+//         for(int i = 2 ;i <=n ; i++){
+//             dp[i] = dp[i/2]+dp[i%2];
+//         }
+//         return dp;
+//     }
+// };
+​
 class Solution {
 public:
     vector<int> countBits(int n) {
@@ -5,10 +21,14 @@ public:
         vector<int> dp(n+1,0);
         
         dp[0] = 0;
-        dp[1] = 1;
         
-        for(int i = 2 ;i <=n ; i++){
-            dp[i] = dp[i/2]+dp[i%2];
+        for(int i = 1 ;i <=n ; i++){
+            if(i%2 == 0){
+                dp[i] = dp[i/2];
+            }
+            else{
+                dp[i] = dp[i-1]+1;
+            }
         }
         return dp;
     }
