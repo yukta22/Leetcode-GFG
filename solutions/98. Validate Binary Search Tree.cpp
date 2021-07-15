418fec801e98584ccde9f8efdbbbaadba2e0fd8d
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-07-15 10:08:12 +0530

    8 ms

--- solutions/98. Validate Binary Search Tree.cpp
+++ solutions/98. Validate Binary Search Tree.cpp
@@ -1,35 +1,48 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// class Solution {
+// public:
+//     vector<int> ans;
+//     void inorder(TreeNode* root){
+//        if (!root)  return;
+​
+//         inorder(root->left);
+//         ans.push_back(root->val);
+//         inorder(root->right);
+//     }
+    
+//     bool isValidBST(TreeNode* root) {
+//         if(!root) return true;
+//         inorder(root);
+        
+//         for(int i = 1 ; i < ans.size() ; i++){
+//             if(ans[i] <= ans[i-1]){
+//                 return false;
+//             }
+//         }
+       
+//         return true;
+//     }
+// };
+​
+////// 2nd  method for reduse S.C //////
 class Solution {
 public:
-    vector<int> ans;
-    void inorder(TreeNode* root){
-       if (!root)  return;
-​
-        inorder(root->left);
-        ans.push_back(root->val);
-        inorder(root->right);
+    bool helper(TreeNode* root ,int *lower , int *upper){
+        if (!root)  return true;
+        
+        if(upper && root->val >= *upper){
+            return false;
+        }
+        
+        if(lower && root->val <= *lower){
+            return false;
+        }
+        return helper(root->left,lower,&(root->val)) && 
+                helper(root->right,&(root->val),upper);
     }
     
     bool isValidBST(TreeNode* root) {
-        if(!root) return true;
-        inorder(root);
-        
-        for(int i = 1 ; i < ans.size() ; i++){
-            if(ans[i] <= ans[i-1]){
-                return false;
-            }
-        }
-       
-        return true;
+       return helper(root,NULL,NULL);        
     }
 };
+​
+​
