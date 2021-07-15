48617bbb22ca27a0bcf6f498bf23d35a49350e20
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-07-15 21:28:45 +0530

    4 ms

---
+++ solutions/338. Counting Bits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        if(n == 0) return {0};
+        vector<int> dp(n+1,0);
+        
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        for(int i = 2 ;i <=n ; i++){
+            dp[i] = dp[i/2]+dp[i%2];
+        }
+        return dp;
+    }
+};
