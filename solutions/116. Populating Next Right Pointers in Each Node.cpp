a75226369a33d9cf3ddb47b36426715e48940c3e
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-07-17 08:55:58 +0530

    16 ms

---
+++ solutions/116. Populating Next Right Pointers in Each Node.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(!root) return root;
+        queue<Node*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            int n = q.size();
+                        
+            for(int i = 0 ; i<n;i++){
+                Node* f = q.front();
+                q.pop();
+                
+                if(i != n-1) f->next = q.front();
+                
+                if(f->left) q.push(f->left);
+                if(f->right) q.push(f->right);
+                
+            }
+            
+        }
+        
+        return root;
+    }
+};
