5d5dd629a322ac3fcd80b7ead189bc2dd2d99953
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-07-30 17:32:06 +0530

    Swapping Nodes in a Linked List[medium]

---
+++ Swapping Nodes in a Linked List.cpp
@@ -0,0 +1,26 @@
+//https://leetcode.com/contest/weekly-contest-223/problems/swapping-nodes-in-a-linked-list/
+
+class Solution {
+public:
+    ListNode* swapNodes(ListNode* head, int k) {
+        ListNode* ptr1 = head;
+        ListNode* ptr2 = head;
+        ListNode* temp = NULL;
+        
+        while(--k){
+            ptr1 = ptr1->next;
+        }
+        
+        temp = ptr1;
+        ptr1 = ptr1->next;
+        
+        while(ptr1){
+            ptr2 = ptr2->next;
+            ptr1 = ptr1->next;
+        }        
+        swap(temp->val , ptr2->val);
+        
+        return head;
+        
+    }
+};
