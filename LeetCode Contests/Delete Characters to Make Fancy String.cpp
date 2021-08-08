049a102ea8082e8fdb5fc27bffa7e117cbbd2df9
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-08-08 11:54:15 +0530

    Delete Characters to Make Fancy String[easy]

---
+++ LeetCode Contests/Delete Characters to Make Fancy String.cpp
@@ -0,0 +1,25 @@
+//https://leetcode.com/contest/biweekly-contest-58/problems/delete-characters-to-make-fancy-string/
+class Solution {
+public:
+    string makeFancyString(string s) {
+        int count = 1;
+        string ans = "";
+        ans += s[0];
+        
+        for(int i = 1 ; i < s.length() ; i++){
+            
+            if(s[i] == s[i-1]){
+                count++;
+            }
+            else{
+                count = 1;
+            }
+            
+            if(count < 3){
+                ans+= s[i];
+            }
+            
+        }
+        return ans;
+    }
+};
