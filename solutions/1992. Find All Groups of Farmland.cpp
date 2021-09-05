9219a28cb5170f6b224fb55cd64773198a5aa147
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-09-05 20:02:53 +0530

    128 ms

---
+++ solutions/1992. Find All Groups of Farmland.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void dfs(vector<vector<int>> &land,int i,int j,int &endi,int &endj){
+        int r = land.size();
+        int c = land[0].size();
+        
+        if(i<0 || j<0 || i>r-1 || j>c-1 || land[i][j] == 0)    return;
+            
+        endi = max(endi,i);
+        endj = max(endj,j);
+        land[i][j] = 0;
+        
+        dfs(land,i-1,j,endi,endj);
+        dfs(land,i+1,j,endi,endj);
+        dfs(land,i,j-1,endi,endj);
+        dfs(land,i,j+1,endi,endj);
+​
+    }
+​
+    
+    vector<vector<int>> findFarmland(vector<vector<int>>& land) {
+        int r = land.size();
+        int c = land[0].size();
+        vector<vector<int>> ans;
+        
+        for(int i = 0 ; i < r ; i ++){
+            for(int j = 0 ; j < c ; j++){
+                if(land[i][j] == 1){
+                    int endi=i,endj=j;
+                    dfs(land,i,j,endi,endj);
+                    ans.push_back({i,j,endi,endj});
+                }
+            }
+        }
+        return ans;
+    }
+};
