1d2dd08d5d3d9d3f5dbef6d400b6fc81d01b136e
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-09-16 22:11:40 +0530

    House Robber III[medium]

--- DFS/House Robber III.cpp
+++ DFS/House Robber III.cpp
@@ -1,6 +1,6 @@
 //https://leetcode.com/problems/house-robber-iii/
 
-///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
 // T.C. : O(2^N) (TLE)
 // S.C. : O(1)
 
@@ -18,3 +18,22 @@ public:
     }
 };
 
+///////////////////////////////////////////////////////////////////////////////////////////////
+// T.C. : O(N) 
+// S.C. : O(n)
+
+class Solution {
+public:
+    map<TreeNode*,int>mpp;
+    int rob(TreeNode* root) {
+        if(root == NULL){
+            return 0;
+        }
+        if(mpp[root])return mpp[root];
+        int val = 0;
+        if(root->left) val += rob(root->left->left ) +  rob(root->left->right);
+        if(root->right) val += rob(root->right->left) + rob(root->right->right);
+        
+        return mpp[root] = max(root->val + val , rob(root->left) + rob(root->right));
+    }
+};
