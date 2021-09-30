0eb43eba1f22f780b21d8adf2e6a4066f1286c31
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-09-30 21:36:40 +0530

    Create Coin Change Permutations.cpp

---
+++ Dynamic Programming/Coin Change Permutations.cpp
@@ -0,0 +1,32 @@
+//https://www.pepcoding.com/resources/online-java-foundation/dynamic-programming-and-greedy/coin-change-permutations-official/ojquestion
+
+#include <bits/stdc++.h>
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int coins[n];
+    for(int i = 0 ; i < n ; i++){
+        cin >> coins[i];
+    }
+    
+    int tar;
+    cin >> tar;
+    vector<int>dp(tar+1,0);
+    dp[0] = 1;
+    
+    for(int amt=1;amt<=tar;amt++){
+        for(int coin : coins){
+            if(coin <= amt){
+                int rem = amt - coin;
+                dp[amt] += dp[rem];
+            }
+        }
+    }
+    
+    cout << dp[tar] <<"\n";
+    return 0;
+}
