d40c18f043bc1bdf97f47916b8970dbfbca5fe11
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-10-16 21:20:34 +0530

    Minimum Number of Moves to Seat Everyone[easy]

---
+++ LeetCode Contests/Minimum Number of Moves to Seat Everyone.cpp
@@ -0,0 +1,21 @@
+//https://leetcode.com/contest/biweekly-contest-63/problems/minimum-number-of-moves-to-seat-everyone/
+
+class Solution {
+public:
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+        
+        int ans  = 0;
+        
+        sort(seats.begin(),seats.end());
+        
+        sort(students.begin(),students.end());
+        
+        for(int i = 0 ; i < students.size();i++){
+            
+            ans += abs(seats[i] - students[i]);
+        }
+        
+        
+        return ans;
+    }
+};
