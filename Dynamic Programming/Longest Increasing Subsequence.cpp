2ce38509a536b3b186759b0c2a0ce57f9fb00e1e
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-10-23 10:56:25 +0530

    Longest Increasing Subsequence [medium]

--- Dynamic Programming/Longest Increasing Subsequence.cpp
+++ Dynamic Programming/Longest Increasing Subsequence.cpp
@@ -1,25 +1,31 @@
-https://leetcode.com/problems/longest-increasing-subsequence/
+//https://leetcode.com/problems/longest-increasing-subsequence/
 
 ///////////////////////////////////////////////////  Brute Force || T.C = O(N^2) || S.C = O(N) ////////////////////////////////////////////////////
 
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        
-        int ans = 1 , n = nums.size();
-        vector<int>dp(n,1);
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < i ; j++){
-                if(nums[i] > nums[j]){
-                    dp[i] = max(dp[i],dp[j]+1);
-                    ans = max(ans,dp[i]);
-                }
-            }
-        }
-        return ans;
+		int n = nums.size();
+
+		vector<int>LIS(n+1, 1);
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < i; j++) {
+				if (nums[i] > nums[j])
+					LIS[i] = max(LIS[i], 1 + LIS[j]);
+			}
+		}
+
+		int ans = 0;
+		for (int i = 0; i < n; i++) {
+			ans = max(ans, LIS[i]);
+		}
+
+		return ans;
+
+
     }
 };
-
 
 
 //////////////////////////////////////////////// Optimal || T.C = O(nlogn) || S.C = O(N) //////////////////////////////////////////
