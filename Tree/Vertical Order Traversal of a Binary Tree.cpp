750dd35dfd440edaca6eca7f1d98afdc2f575879
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-10-28 23:41:01 +0530

    Update Vertical Order Traversal of a Binary Tree.cpp

--- Tree/Vertical Order Traversal of a Binary Tree.cpp
+++ Tree/Vertical Order Traversal of a Binary Tree.cpp
@@ -1,47 +1,35 @@
-  /////////////////////////////////////////////////////////////// Hard //////////////////////////////////////////////////////////////
-
-
- 
- /////////////////////////////////////////////////////////////// Using BFS /////////////////////////////////////////////////////////////////////////////////////
- /////////////////////////////////////////////////////// Time : O(logn)*O(logn)*O(logn)*O(n) for both approch ///////////////////////////////////////////////////////////////////////////
-
-  vector<vector<int>> verticalTraversal(TreeNode* root) {
-        TreeNode*curr;
-        queue<pair<TreeNode*,int>>q;
-        q.push({root,0});
-        vector<vector<int>>ans;
-        map<int ,vector<int>>mymap;
-        
-        while(!q.empty()){
-            int size = q.size();
-            map<int,set<int>>mapset;
-            
-            while(size--){
-                curr = q.front().first;
-                int col = q.front().second;
-                q.pop();
-                
-                mapset[col].insert(curr->val);
-                
-                if(curr->left)
-                    q.push({curr->left,col-1});
-                
-                if(curr->right)
-                    q.push({curr->right,col+1});
-                                
+//https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/submissions/
+class Solution {
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        map<int, map<int, multiset<int>>> nodes;
+        queue<pair<TreeNode*, pair<int, int>>> q;
+        q.push({root, {0, 0}});
+        while (!q.empty()) {
+            auto f = q.front();
+            q.pop();
+            TreeNode* node = f.first;
+            int x = f.second.first, y = f.second.second;
+            nodes[x][y].insert(node -> val);
+            if (node -> left) {
+                q.push({node -> left, {x - 1, y + 1}});
             }
-             for(auto it: mapset)
-                for(auto it2: it.second)
-                    mymap[it.first].push_back(it2);
-            
+            if (node -> right) {
+                q.push({node -> right, {x + 1, y + 1}});
+            }
         }
-         for(auto it: mymap)
-            ans.push_back(it.second);
         
+        vector<vector<int>> ans;
+        for (auto p : nodes) {
+            vector<int> col;
+            for (auto q : p.second) {
+                col.insert(col.end(), q.second.begin(), q.second.end());
+            }
+            ans.push_back(col);
+        }
         return ans;
-        
     }
-
+};
 
    ////////////////////////////////////////////////////////////////  DFS  ////////////////////////////////////////////////////////////////////////////////
  
