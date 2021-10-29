4c8007c98f3949588546c3292de6c4336fc4de8d
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-10-29 10:00:51 +0530

    Binary Tree Right Side View [Medium]

--- Tree/Binary Tree Right Side View.cpp
+++ Tree/Binary Tree Right Side View.cpp
@@ -1,22 +1,52 @@
-https://leetcode.com/problems/binary-tree-right-side-view/
-
+//https://leetcode.com/problems/binary-tree-right-side-view/
 
 class Solution {
-    vector<int>ans;
-    void solve(TreeNode * root , int *max , int level){        
-        if(!root)return;        
-        if(*max < level){
-           ans.push_back(root->val);
-           *max = level;            
-        }        
-        solve(root->right,max,level+1);
-        solve(root->left,max,level+1);        
-    }
-      
 public:
+    
+    void solve(TreeNode* root, int level, vector<int>& ans){
+        if(root == NULL) return;
+        
+        if(ans.size() == level){
+            ans.push_back(root->val);
+        }
+        
+        solve(root->right , level+1,ans);
+        solve(root->left , level+1,ans);
+        
+        
+    }
+    
     vector<int> rightSideView(TreeNode* root) {
-        int max = 0;      
-        solve(root,&max,1);   
+        vector<int> ans;
+        solve(root,0,ans);
         return ans;
+    }
+};
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Solution {
+public:
+    
+    void rightSideViewHelper(TreeNode* root , int level , int *max , vector<int> &v){
+        if(root == NULL) return;
+        
+        if(*max < level){
+            v.push_back(root->val);
+            *max = level;
+        }
+        
+        rightSideViewHelper(root->right,level+1,max,v);
+        rightSideViewHelper(root->left,level+1,max,v);
+        
+    }
+    
+    
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> v;
+        int max=0;
+        rightSideViewHelper(root,1,&max,v);
+        return v;
     }
 };
