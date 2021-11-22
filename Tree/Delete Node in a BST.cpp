10818b8b4e606a85cfee4fefed7c28e12552b220
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-11-22 23:28:12 +0530

    Delete Node in a BST [Medium]

---
+++ Tree/Delete Node in a BST.cpp
@@ -0,0 +1,58 @@
+//https://leetcode.com/problems/delete-node-in-a-bst/
+
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root == NULL){
+            return NULL;
+        }
+        
+        if(root->val == key){
+            return helper(root);
+        }
+        TreeNode* dummy = root;
+        
+        while(root != NULL){
+            if(root->val > key){
+                if(root->left != NULL && root->left->val == key){
+                    root->left = helper(root->left);
+                }
+                else{
+                   root = root->left;
+                }
+            }
+            else{
+                if(root->right != NULL && root->right->val == key){
+                    root->right = helper(root->right);
+                }
+                else{
+                   root = root->right;
+                }                
+            }
+        }
+        return dummy;
+    }
+    
+    TreeNode* helper(TreeNode* root) {
+        if (root->left == NULL){
+            return root->right;
+        } 
+        
+        else if (root->right == NULL){
+            return root->left;
+        } 
+        
+        TreeNode* rightChild = root->right;
+        TreeNode* lastRight = findLastRight(root->left);
+        lastRight->right = rightChild;
+        return root->left;
+    }
+    
+    TreeNode* findLastRight(TreeNode* root) {
+        if (root->right == NULL) {
+            return root;
+        }
+        return findLastRight(root->right);
+    }
+
+};
