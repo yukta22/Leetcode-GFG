0716dd963505af42a036a432e76fda5aaa038574
yukta22 <60733741+yukta22@users.noreply.github.com>
2022-01-08 21:50:09 +0530

    Capitalize the Title [easy]

---
+++ LeetCode Contests/Capitalize the Title.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string capitalizeTitle(string title) {
+        title.push_back(' ');
+        string ans="";
+        for(int i=0; i<title.length(); i++){
+            string temp;
+            for(; title[i]!=' '; i++) {
+                temp += tolower(title[i]);
+            }
+            if(temp.length() > 2) temp[0] = toupper(temp[0]);
+            
+            if(ans == "") ans += temp;
+            else ans += " " + temp;
+            
+        }
+        return ans;
+
+    }
+};
