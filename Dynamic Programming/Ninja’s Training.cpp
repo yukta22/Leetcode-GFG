de91541cc4bea6c49c4415240ca902eb0daa780a
yukta22 <60733741+yukta22@users.noreply.github.com>
2022-01-21 19:17:25 +0530

    Ninja’s Training [medium]

---
+++ Dynamic Programming/Ninja’s Training.cpp
@@ -0,0 +1,64 @@
+//https://www.codingninjas.com/codestudio/problems/ninja-s-training_3621003?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos&leftPanelTab=0
+
+
+//////////////////////////////////////////////////////////// memoization ////////////////////////////////////////////////////////////
+
+int solve(int day, int last, vector<vector<int>> &points, vector<vector<int>> &dp){
+    
+    if(day == 0){
+        int maxi = 0;
+        for(int task = 0 ; task < 3 ; task++){
+            if(task != last){
+                maxi = max(maxi, points[0][task]);
+            }
+        }
+        return maxi;
+    }
+    
+    if(dp[day][last] != -1)  return dp[day][last];
+    
+    int maxi = 0;
+    for(int task = 0 ; task < 3 ; task++){
+        if(task != last){
+            int point = points[day][task] + solve(day-1, task, points, dp);
+            maxi = max(maxi, point);
+        }
+    }
+     return dp[day][last] = maxi;   
+}
+
+int ninjaTraining(int n, vector<vector<int>> &points)
+{
+    vector<vector<int>> dp(n, vector<int> (4,-1));
+    return solve(n-1, 3 , points , dp);
+}
+
+//////////////////////////////////////////////////////////// Tabulation ////////////////////////////////////////////////////////////
+
+int ninjaTraining(int n, vector<vector<int>> &points)
+{
+      vector < vector < int > > dp(n, vector < int > (4, 0));
+
+  dp[0][0] = max(points[0][1], points[0][2]);
+  dp[0][1] = max(points[0][0], points[0][2]);
+  dp[0][2] = max(points[0][0], points[0][1]);
+  dp[0][3] = max(points[0][0], max(points[0][1], points[0][2]));
+
+  for (int day = 1; day < n; day++) {
+    for (int last = 0; last < 4; last++) {
+      dp[day][last] = 0;
+      for (int task = 0; task <= 2; task++) {
+        if (task != last) {
+          int activity = points[day][task] + dp[day - 1][task];
+          dp[day][last] = max(dp[day][last], activity);
+        }
+      }
+    }
+
+  }
+
+    return dp[n-1][3];
+}
+
+
+
